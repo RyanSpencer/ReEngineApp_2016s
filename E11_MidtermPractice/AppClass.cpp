000447e2f3fc527ef1f0588f8cf9408b63b94c3f
Ryan Spencer (RIT Student) <rxs2833@ad.rit.edu>
2017-03-23 14:49:45 -0400

    Fixed rotation

--- E11_MidtermPractice/AppClass.cpp
+++ E11_MidtermPractice/AppClass.cpp
@@ -16,13 +16,7 @@ void AppClass::Update(void)
 #pragma endregion
 #pragma region YOUR CODE GOES HERE
 	//total loop is the amount of degrees that each loop should increase based on the amount of time it takes to compelte a loop divided by the time each loop takes
-	totalLoop = 360/ (5.0f / dDeltaTime);
-	//Add it to loop per each loop
-	loop += totalLoop;
-	//Reset after 360
-	if (loop > 360) {
-		loop = 0;
-	}
+	float apercent = MapValue((float)(dTotalTime / dDeltaTime), 0.0f, (float)(5.0f / dDeltaTime), 0.0f, (float)(360));
 	if (dTotalTime > 5) {
 		dTotalTime = 0;
 		if (!reverse) reverse = true;
@@ -36,7 +30,7 @@ void AppClass::Update(void)
 	else {
 		lerpedVec = glm::lerp(vector3(0.0f, 5.0f, 0.0f), vector3(0.0f, 0.0f, 0.0f), fpercent);
 	}
-	m_m4Steve = glm::rotate((float)(loop), REAXISZ) * glm::translate(lerpedVec);
+	m_m4Steve = glm::rotate((float)(apercent), REAXISZ) * glm::translate(lerpedVec);
 
 	//m_m4Steve = glm::mat4(1.0f); // same as m_m4Steve = IDENTITY_M4; setting the identity to steve
 #pragma endregion
